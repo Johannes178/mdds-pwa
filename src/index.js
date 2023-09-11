69b2032d56208b614a21c6c8b231df6bbc06a4eb
JJ <69968451+Johannes178@users.noreply.github.com>
2023-09-11 23:05:24 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const http = require('http');
-const server = http.createServer(app);
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
 
-app.get('/', (req, res) => {
-  res.send('<h1>Hello world</h1>');
+app.use(express.static("public"));
+
+io.on("connection", (socket) => {
+  console.log("a user connected", socket.id);
+  socket.on("disconnect", () => {
+    console.log("a user disconnected", socket.id);
+    //TODO: remove user with socket.id from users array
+  });
+
+  socket.on("chat message", (msg) => {
+    console.log("message: ", msg);
+    io.emit("chat message", msg);
+  });
 });
-
-server.listen(3000, () => {
-  console.log('listening on *:3000');
+http.listen(5502, () => {
+  console.log("port is 5502");
 });
