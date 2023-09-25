747d95de4ea84c38d82b24ee63ee4b8218b89c72
Johannes Jokinen <johannes.jokinen@live.com>
2023-09-25 20:53:06 +0300

    ja tuosta

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,13 @@ module.exports = {
   content: ["./public/**/*.{html,js}", "./src/**/*.{html,js}"],
   theme: {
     screens: {
+      xs: "390px",
       sm: "640px",
       md: "768px",
       lg: "1024px",
       xl: "1280px",
       "2xl": "1536px",
+      "3xl": "1920px",
     },
     extend: {
       backgroundImage: {
